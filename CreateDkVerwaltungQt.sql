f856b5d85f9b64616219a29ba6de15b418e65024
Volker <VPorzelt@gmx.de>
2017-01-03 03:08:13 +0100

    Sortierung
    Nach dem Import Tabelle DkVerwaltung sortieren, damit beim Update der
    jeweils aktuellste Personen-Datensatz genommen wird

--- CreateDkVerwaltungQt.sql
+++ CreateDkVerwaltungQt.sql
@@ -1,20 +1,23 @@
 /* sqlite3 DkVerwaltungQt.db3 < CreateDkVerwaltungQt.sql */
-create table DKVerwaltung (Datum TEXT, DKNr TEXT, Vorname TEXT, Name TEXT, Anrede TEXT, DKNummer TEXT, Straße TEXT, PLZ TEXT, Ort TEXT, Email TEXT, Rueckzahlung TEXT, vorgemerkt TEXT, Betrag TEXT, Zinssatz TEXT, Bemerkung TEXT);
+CREATE TABLE DKVerwaltungOrg (Datum TEXT, DKNr TEXT, Vorname TEXT, Name TEXT, Anrede TEXT, DKNummer TEXT, Straße TEXT, PLZ TEXT, Ort TEXT, Email TEXT, Rueckzahlung TEXT, vorgemerkt TEXT, Betrag TEXT, Zinssatz TEXT, Bemerkung TEXT);
 .separator ","
-.import DK-Verwaltung.csv DKVerwaltung
-create table DKPersonen (PersonId INTEGER PRIMARY KEY AUTOINCREMENT, Vorname TEXT, Name TEXT, Anrede TEXT, Straße TEXT, PLZ TEXT, Ort TEXT, Email TEXT);
-create table DKBuchungen (BuchungId INTEGER PRIMARY KEY AUTOINCREMENT, PersonId INTEGER, Datum TEXT, DKNr TEXT, DKNummer TEXT, Rueckzahlung TEXT, vorgemerkt TEXT, Betrag REAL, Zinssatz REAL, Bemerkung TEXT, FOREIGN KEY(PersonId) REFERENCES DKPerson(PersonId));
+.header on
+.import DK-Verwaltung_F13_Turley_GmbH.csv DKVerwaltungOrg
+CREATE TABLE DKVerwaltung (Datum TEXT, DKNr TEXT, Vorname TEXT, Name TEXT, Anrede TEXT, DKNummer TEXT, Straße TEXT, PLZ TEXT, Ort TEXT, Email TEXT, Rueckzahlung TEXT, vorgemerkt TEXT, Betrag TEXT, Zinssatz TEXT, Bemerkung TEXT);
+INSERT INTO DKVerwaltung SELECT * FROM DKVerwaltungOrg ORDER BY CAST(DkNr AS INTEGER);
+DROP table DKVerwaltungORG;
+CREATE TABLE DKPersonen (PersonId INTEGER PRIMARY KEY AUTOINCREMENT, Vorname TEXT, Name TEXT, Anrede TEXT, Straße TEXT, PLZ TEXT, Ort TEXT, Email TEXT);
+CREATE TABLE DKBuchungen (BuchungId INTEGER PRIMARY KEY AUTOINCREMENT, PersonId INTEGER, Datum TEXT, DKNr TEXT, DKNummer TEXT, Rueckzahlung TEXT, vorgemerkt TEXT, Betrag REAL, Zinssatz REAL, Bemerkung TEXT, FOREIGN KEY(PersonId) REFERENCES DKPerson(PersonId));
 INSERT INTO DKPersonen (Vorname, Name, Anrede, Straße, PLZ, Ort, Email)
-SELECT Vorname, Name, Anrede, Straße, PLZ, Ort, Email FROM DKVerwaltung;
+SELECT                  Vorname, Name, Anrede, Straße, PLZ, Ort, Email FROM DKVerwaltung;
 INSERT INTO DKBuchungen (PersonId, Datum, DKNr, DKNummer, Rueckzahlung, vorgemerkt, Betrag, Zinssatz, Bemerkung)
-SELECT                      ROWID, Datum, DKNr, DKNummer, Rueckzahlung, vorgemerkt, Betrag, Zinssatz, Bemerkung 
-FROM DKVerwaltung;
+SELECT                      ROWID, Datum, DKNr, DKNummer, Rueckzahlung, vorgemerkt, Betrag, Zinssatz, Bemerkung FROM DKVerwaltung;
 DROP table DKVerwaltung;
 UPDATE DkBuchungen SET Betrag = replace(replace(replace(Betrag, ' €', ''), '.', ''), ',', '.');
 /* UPDATE DkBuchungen SET Betrag = replace(replace(Betrag, ' €', ''), '.', ''); */
 UPDATE DkBuchungen SET Zinssatz = replace(replace(Zinssatz, '%', ''), ',', '.');
 /* UPDATE DkBuchungen SET Zinssatz = replace(Zinssatz, '%', ''); */
 /* UPDATE DkBuchungen SET Zinssatz = replace(Zinssatz, '.', ','); */
-create table DKZinssaetze (Zinssatz REAL PRIMARY KEY, Beschreibung TEXT);
+CREATE TABLE DKZinssaetze (Zinssatz REAL PRIMARY KEY, Beschreibung TEXT);
 INSERT INTO DKZinssaetze (Zinssatz)
-SELECT Zinssatz from DkBuchungen GROUP BY Zinssatz ORDER BY Zinssatz;
+SELECT Zinssatz FROM DkBuchungen GROUP BY Zinssatz ORDER BY Zinssatz;
--- UpdateDkVerwaltungQt.sql
+++ UpdateDkVerwaltungQt.sql
@@ -1,5 +1,5 @@
 /* sqlite3 DkVerwaltungQt.db3 < UpdateDkVerwaltungQt.sql */
-update DkBuchungen set PersonId = (select mpid from (Select t.PersonId AS pid, xx.MaxPersonId as mpid, t.Vorname , t.Name from DkPersonen t inner join ( Select max(PersonId) AS MaxPersonId, Vorname, Name from DkPersonen group by Vorname, Name) xx on t.Vorname = xx.Vorname AND t.Name = xx.Name ORDER BY PersonID) where DkBuchungen.PersonId = pid);
+UPDATE DkBuchungen SET PersonId = (select mpid from (Select t.PersonId AS pid, xx.MaxPersonId as mpid, t.Vorname , t.Name from DkPersonen t inner join ( Select max(PersonId) AS MaxPersonId, Vorname, Name from DkPersonen group by Vorname, Name) xx on t.Vorname = xx.Vorname AND t.Name = xx.Name ORDER BY PersonID) where DkBuchungen.PersonId = pid);
 DELETE FROM DkPersonen WHERE NOT EXISTS (SELECT * FROM DkBuchungen WHERE DkBuchungen.PersonId = DkPersonen.PersonId);
 ALTER TABLE DkBuchungen ADD COLUMN Anfangsdatum TEXT;
 ALTER TABLE DkBuchungen ADD COLUMN AnfangsBetrag REAL;

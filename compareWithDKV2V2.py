1a7ee80025c8b635dd651c3609a9e00bcba29580
vopor <VPorzelt@gmx.de>
2020-11-14 19:36:11 +0100

    Vertraege mit mehrere Buchungen

--- compareWithDKV2V2.py
+++ compareWithDKV2V2.py
@@ -27,12 +27,6 @@ if not os.path.isfile(DKV2_db3_file):
     print DKV2_db3_file + "existiert nicht."
     exit(3)
 
-Year2 = "19"
-withYear=False
-if len(sys. argv) == 4:
-    Year2 = sys.argv[3]
-    withYear=True
-verbose=False
 try:
     conn = sqlite3.connect('')
     stmt = conn.cursor()
@@ -58,6 +52,18 @@ try:
     select_stmt = 'SELECT COUNT(*) FROM db_to.Vertraege'
     stmt.execute(select_stmt)
     print stmt.fetchone()[0]
+    # Summierte DkBuchungen und Vertraege
+    print "Anzahl DKBuchungen:"
+    select_stmt = 'SELECT COUNT(*) FROM db_from.DKBuchungen '
+    # select_stmt += 'WHERE Anfangsdatum <> "" AND DkNummer <> "Stammkapital" '
+    stmt.execute(select_stmt)
+    print stmt.fetchone()[0]
+    print "Anzahl Buchungen:"
+    select_stmt = 'SELECT COUNT(*) FROM db_to.Buchungen'
+    stmt.execute(select_stmt)
+    print stmt.fetchone()[0]
+
+
     
     print "Vertraege, die nicht übernommen wurden:"
     select_stmt = 'SELECT * FROM db_from.DKBuchungen '
@@ -89,135 +95,10 @@ try:
     
     sys.exit(0)
 
-    '''
-    print "Summe DkBuchungen ohne Rückzahlung:"
-    select_stmt = 'SELECT COUNT(*), SUM(db_from.DKBuchungen.Betrag) FROM db_from.DKBuchungen WHERE Rueckzahlung = "" '
-    select_stmt += 'AND DkNummer <> "Stammkapital" '
-    stmt.execute(select_stmt)
-    result = stmt.fetchone()
-    AnzahlOhneRueckzahlung = result[0]
-    SummeOhneRueckzahlung = 0
-    if result[1] != None: SummeOhneRueckzahlung = result[1]
-    print AnzahlOhneRueckzahlung
-    print SummeOhneRueckzahlung
-
-    print "Summe Vertraege ohne Rückzahlung:"
-    select_stmt = 'SELECT COUNT(*), SUM(db_to.Vertraege.Betrag) FROM db_to.Vertraege WHERE LetzteZinsberechnung IS NULL '
-    stmt.execute(select_stmt)
-    result = stmt.fetchone()
-    print result[0]
-    print result[1]
-
-    print "Summe DkBuchungen mit Rückzahlung:"
-    select_stmt = 'SELECT COUNT(*), (SUM(db_from.DKBuchungen.Betrag) * -1) '
-    select_stmt += 'FROM db_from.DKBuchungen WHERE Rueckzahlung <> "" '
-    select_stmt += 'AND (Anfangsdatum IS NULL OR Anfangsdatum = "") '
-    select_stmt += 'AND Betrag < 0 '
-    if withYear: select_stmt += 'AND (substr(Rueckzahlung ,7,2) = "' + Year2 + '")'
-    select_stmt += 'AND DkNummer <> "Stammkapital"'
-    stmt.execute(select_stmt)
-    result = stmt.fetchone()
-    AnzahlMitRueckzahlung = result[0]
-    SummeMitRueckzahlung = 0
-    if result[1] != None: SummeMitRueckzahlung = result[1]
-    print str(AnzahlMitRueckzahlung)
-    print str(SummeMitRueckzahlung)
-
-    print "Summe Vertraege mit Rückzahlung:"
-    select_stmt = 'SELECT COUNT(*), SUM(db_to.Vertraege.Wert) '
-    select_stmt += 'FROM db_to.Vertraege WHERE LetzteZinsberechnung NOT NULL  AND WERT <> 0 '
-    if withYear: select_stmt += 'AND LetzteZinsberechnung >= "20' + Year2 + '-01-01" '
-    stmt.execute(select_stmt)
-    result = stmt.fetchone()
-    print result[0]
-    print result[1]
-
-    if verbose:
-        print "DkBuchungen mit Rückzahlung:"
-        select_stmt = 'SELECT DkNummer, Betrag, Rueckzahlung '
-        select_stmt += 'FROM db_from.DKBuchungen WHERE Rueckzahlung <> "" '
-        select_stmt += 'AND (Anfangsdatum IS NULL OR Anfangsdatum = "") '
-        select_stmt += 'AND Betrag < 0 '
-        if withYear: select_stmt += 'AND (substr(Rueckzahlung ,7,2) = "' + Year2 + '")'
-        select_stmt += 'AND DkNummer <> "Stammkapital"'
-        stmt.execute(select_stmt)
-        buchungen = stmt.fetchall()
-        for row in buchungen:
-            print row
-    if verbose:
-        print "Vertraege mit Rückzahlung:"
-        select_stmt = 'SELECT Kennung, Wert, LetzteZinsberechnung '
-        select_stmt += 'FROM db_to.Vertraege WHERE  LetzteZinsberechnung NOT NULL AND WERT <> 0 '
-        if withYear: select_stmt += 'AND LetzteZinsberechnung >= "20' + Year2 + '-01-01" '
-        stmt.execute(select_stmt)
-        vertraege = stmt.fetchall()
-        for row in vertraege:
-            print row
-
-    print 
-    print "--------------"
-    print 
-
-    print "Summe DkBuchungen Betrag:"
-    print AnzahlOhneRueckzahlung + AnzahlMitRueckzahlung
-    print SummeOhneRueckzahlung + SummeMitRueckzahlung
-
-    print "Summe Vertraege Wert:"
-    select_stmt = 'SELECT COUNT(*), SUM(db_to.Vertraege.Wert) FROM db_to.Vertraege '
-    select_stmt += 'WHERE Wert <> 0 '
-    # select_stmt += 'LetzteZinsberechnung IS NULL AND '
-    stmt.execute(select_stmt)
-    result = stmt.fetchone()
-    print result[0]
-    print result[1]
-
-    if False:
-        print "Vertraege, wo der Wert nicht mit dem Betrag von DkBuchungen übereinstimmt:"
-        select_stmt = 'SELECT DkNummer, Sum(Betrag) AS SumBetrag FROM db_from.DKBuchungen GROUP BY db_from.DKBuchungen.DkNummer HAVING '
-        # select_stmt += 'Anfangsdatum <> "" AND Anfangsdatum NOT NULL AND 
-        select_stmt += 'SumBetrag <> '
-        select_stmt += '('
-        select_stmt += 'SELECT Wert FROM db_to.Vertraege WHERE db_from.DKBuchungen.DkNummer = db_to.Vertraege.Kennung'    
-        select_stmt += ') '
-        stmt.execute(select_stmt)
-        buchungen = stmt.fetchall()
-        for row in buchungen:
-            print row
-
-        print "DkBuchungen, wo der Betrag nicht mit dem Wert von Vertraege übereinstimmt:"
-        select_stmt = 'SELECT Kennung, Wert FROM db_to.Vertraege WHERE Wert <> '
-        select_stmt += '('
-        select_stmt += 'SELECT Betrag FROM db_from.DKBuchungen '
-        select_stmt += 'WHERE Anfangsdatum <> "" AND Anfangsdatum NOT NULL AND '
-        select_stmt += 'db_from.DKBuchungen.DkNummer = db_to.Vertraege.Kennung'        
-        select_stmt += ')'
-        stmt.execute(select_stmt)
-        buchungen = stmt.fetchall()
-        for row in buchungen:
-            print row
-
-    print "Summe DkBuchungen AnfangsBetrag:"
-    select_stmt = 'SELECT Count(*), SUM(db_from.DKBuchungen.AnfangsBetrag) FROM db_from.DKBuchungen '
-    select_stmt += 'WHERE Anfangsdatum <> "" AND Anfangsdatum NOT NULL AND DkNummer <> "Stammkapital" '
-    # select_stmt += 'AND Rueckzahlung = ""'
-    stmt.execute(select_stmt)
-    result = stmt.fetchone()
-    print result[0]
-    print result[1]
-
-    print "Summe Vertraege Betrag:"
-    select_stmt = 'SELECT COUNT(*), SUM(db_to.Vertraege.Betrag) FROM db_to.Vertraege '
-    # select_stmt += 'WHERE LetzteZinsberechnung IS NULL '
-    stmt.execute(select_stmt)
-    result = stmt.fetchone()
-    print result[0]
-    print result[1]
-    '''
-
 except SystemExit:
     pass
 except:
     print "Unexpected error:", sys.exc_info()[0]
     print sys.exc_info()
 
-exit(0)
+sys.exit(0)
--- exportToDKV2V2.py
+++ exportToDKV2V2.py
@@ -16,8 +16,8 @@ import sqlite3
 # print sqlite3.version
 # print sys.argv
 #print len(sys. argv)
-if len(sys. argv) < 4:
-    print "Aufruf: ./exportToDKV2.py <DkVerwaltungQt-db3-file> <DKV2-db3-file> <Year2>"
+if len(sys. argv) < 3:
+    print "Aufruf: ./exportToDKV2.py <DkVerwaltungQt-db3-file> <DKV2-db3-file> "
     sys.exit(1)
 
 DkVerwaltungQt_db3_file=sys.argv[1]
@@ -28,8 +28,6 @@ if not os.path.isfile(DkVerwaltungQt_db3_file):
 if not os.path.isfile(DKV2_db3_file):
     print DKV2_db3_file + "existiert nicht."
     sys.exit(3)
-
-Year2 = sys.argv[3]
 
 try:
     conn = sqlite3.connect('')
@@ -47,226 +45,103 @@ try:
     buchungen = stmt.fetchall()
     if len(buchungen) > 0:
         due_direkt_nach_jahresabschluss = False
+        print
         print "Es existieren mehrere Buchungen pro Vertrag"
-        print "buchungen", len(buchungen)
-        for row in buchungen:
-            print row
-        sys.exit(4)
+        print
+        # print "buchungen", len(buchungen)
+        # for row in buchungen:
+        #     print row
+        # sys.exit(4)
     else:
         print
         print "Datenübernahme direkt nach dem Jahreswechsel"
         print
 
-    print "Kreditoren hinzufügen"
-    insert_stmt = 'INSERT INTO db_to.Kreditoren (id, Vorname, Nachname, Strasse, Plz, Stadt, EMail, Anmerkung, IBAN, Bic) '
-    insert_stmt += 'SELECT Personid, Vorname, Name, Straße, PLZ, Ort, Email, "", "", "" FROM db_from.DKPersonen'
-    stmt.execute(insert_stmt);
-    print "Anzahl: ", stmt.rowcount
-    conn.commit()
-    
-    print "Vertraege hinzufügen"
-    insert_stmt = 'INSERT INTO db_to.Vertraege (id, KreditorId, Kennung, ZSatz, Betrag, thesaurierend, Vertragsdatum, LaufzeitEnde, Kfrist) '
-    insert_stmt += 'SELECT BuchungId, PersonId, DKNummer, db_from.DKBuchungen.Zinssatz*100, AnfangsBetrag*100, 1, '
-    insert_stmt += '"20" || substr(Anfangsdatum,7,2) || "-" || substr(Anfangsdatum,4,2) || "-" || substr(Anfangsdatum,1,2), '
-    insert_stmt += 'CASE WHEN (vorgemerkt != "") THEN ("20" || substr(vorgemerkt,7,2) || "-" || substr(vorgemerkt,4,2) || "-" || substr(vorgemerkt,1,2)) ELSE "3000-12-31" END, '
-    insert_stmt += 'CASE WHEN (vorgemerkt != "") THEN -1 ELSE 6 END '
-    insert_stmt += 'FROM db_from.DKBuchungen '
-    # insert_stmt += 'WHERE Anfangsdatum <> "" AND DkNummer <> "Stammkapital" '
-    # insert_stmt += 'AND Rueckzahlung = ""'
-    stmt.execute(insert_stmt);
-    print "Anzahl: ", stmt.rowcount
-    conn.commit()
-
-    print "Buchungen hinzufügen"
-    insert_stmt = 'INSERT INTO db_to.Buchungen (id, VertragsId, Datum, Buchungsart, Betrag) '
-    insert_stmt += 'SELECT a.BuchungId, a.BuchungId, '
-    insert_stmt += '("20" || substr(a.Datum,7,2) || "-" || substr(a.Datum,4,2) || "-" || substr(a.Datum,1,2)), '
-    insert_stmt += 'CASE WHEN (a.Betrag >= 0) THEN 1 ELSE 2 END, a.Betrag*100 '
-    insert_stmt += 'FROM db_from.DKBuchungen a '
-    # insert_stmt += 'WHERE Anfangsdatum <> "" AND DkNummer <> "Stammkapital" '
-    stmt.execute(insert_stmt);
-    print "Anzahl: ", stmt.rowcount
-    conn.commit()
-
+    # due_direkt_nach_jahresabschluss = False
     if due_direkt_nach_jahresabschluss:
-        sys.exit(0)
+        
+        print "Kreditoren hinzufügen"
+        insert_stmt = 'INSERT INTO db_to.Kreditoren (id, Vorname, Nachname, Strasse, Plz, Stadt, EMail, Anmerkung, IBAN, Bic) '
+        insert_stmt += 'SELECT Personid, Vorname, Name, Straße, PLZ, Ort, Email, "", "", "" FROM db_from.DKPersonen'
+        stmt.execute(insert_stmt);
+        print "Anzahl: ", stmt.rowcount
+        conn.commit()
+    
+        print "Vertraege hinzufügen"
+        insert_stmt = 'INSERT INTO db_to.Vertraege (id, KreditorId, Kennung, ZSatz, Betrag, thesaurierend, Vertragsdatum, LaufzeitEnde, Kfrist) '
+        insert_stmt += 'SELECT BuchungId, PersonId, DKNummer, db_from.DKBuchungen.Zinssatz*100, AnfangsBetrag*100, 1, '
+        insert_stmt += '"20" || substr(Anfangsdatum,7,2) || "-" || substr(Anfangsdatum,4,2) || "-" || substr(Anfangsdatum,1,2), '
+        insert_stmt += 'CASE WHEN (vorgemerkt != "") THEN ("20" || substr(vorgemerkt,7,2) || "-" || substr(vorgemerkt,4,2) || "-" || substr(vorgemerkt,1,2)) ELSE "3000-12-31" END, '
+        insert_stmt += 'CASE WHEN (vorgemerkt != "") THEN -1 ELSE 6 END '
+        insert_stmt += 'FROM db_from.DKBuchungen '
+        # insert_stmt += 'WHERE Rueckzahlung = ""'
+        stmt.execute(insert_stmt);
+        print "Anzahl: ", stmt.rowcount
+        conn.commit()
 
-    '''
-    if False:
-        print "AnfangsBuchungen hinzufügen"
+        print "Buchungen hinzufügen"
         insert_stmt = 'INSERT INTO db_to.Buchungen (id, VertragsId, Datum, Buchungsart, Betrag) '
         insert_stmt += 'SELECT a.BuchungId, a.BuchungId, '
-        # insert_stmt += 'date( ("20" || substr(a.AnfangsDatum,7,2) || "-" || substr(a.AnfangsDatum,4,2) || "-" || substr(a.AnfangsDatum,1,2)), "-1 day"), '
-        insert_stmt += '("20" || substr(a.AnfangsDatum,7,2) || "-" || substr(a.AnfangsDatum,4,2) || "-" || substr(a.AnfangsDatum,1,2)), '
-        insert_stmt += 'CASE WHEN (a.Betrag >= 0) THEN 1 ELSE 2 END, a.AnfangsBetrag*100 '
+        insert_stmt += '("20" || substr(a.Datum,7,2) || "-" || substr(a.Datum,4,2) || "-" || substr(a.Datum,1,2)), '
+        insert_stmt += 'CASE WHEN (a.Betrag >= 0) THEN 1 ELSE 2 END, a.Betrag*100 '
         insert_stmt += 'FROM db_from.DKBuchungen a '
-        insert_stmt += 'WHERE Anfangsdatum <> "" AND DkNummer <> "Stammkapital" '
         stmt.execute(insert_stmt);
         print "Anzahl: ", stmt.rowcount
         conn.commit()
 
         sys.exit(0)
 
-    update_stmt = 'UPDATE db_to.Buchungen SET VertragsId = '
-    update_stmt += '('
-    update_stmt += 'SELECT c.id FROM db_from.DKBuchungen b, db_to.Vertraege c '
-    update_stmt += 'WHERE db_to.Buchungen.id = b.BuchungId '
-    update_stmt += 'AND b.DkNummer = c.Kennung '
-    update_stmt += ')'
-
-    stmt.execute(update_stmt);
+    if not due_direkt_nach_jahresabschluss:
+        print "Kreditoren hinzufügen"
+        insert_stmt = 'INSERT INTO db_to.Kreditoren (id, Vorname, Nachname, Strasse, Plz, Stadt, EMail, Anmerkung, IBAN, Bic) '
+        insert_stmt += 'SELECT Personid, Vorname, Name, Straße, PLZ, Ort, Email, "", "", "" FROM db_from.DKPersonen'
+        stmt.execute(insert_stmt);
+        print "Anzahl: ", stmt.rowcount
+        conn.commit()
     
-    print "Anzahl: ", stmt.rowcount
-    # vertraege = stmt.fetchall()
-    # print "vertraege", len(vertraege)
-    # for row in vertraege:
-    #     print row
-    conn.commit()
+        print "Vertraege hinzufügen"
+        insert_stmt = 'INSERT INTO db_to.Vertraege (id, KreditorId, Kennung, ZSatz, Betrag, thesaurierend, Vertragsdatum, LaufzeitEnde, Kfrist) '
+        insert_stmt += 'SELECT BuchungId, PersonId, DKNummer, db_from.DKBuchungen.Zinssatz*100, AnfangsBetrag*100, 1, '
+        insert_stmt += '"20" || substr(Anfangsdatum,7,2) || "-" || substr(Anfangsdatum,4,2) || "-" || substr(Anfangsdatum,1,2), '
+        insert_stmt += 'CASE WHEN (vorgemerkt != "") THEN ("20" || substr(vorgemerkt,7,2) || "-" || substr(vorgemerkt,4,2) || "-" || substr(vorgemerkt,1,2)) ELSE "3000-12-31" END, '
+        insert_stmt += 'CASE WHEN (vorgemerkt != "") THEN -1 ELSE 6 END '
+        insert_stmt += 'FROM db_from.DKBuchungen '
+        insert_stmt += 'WHERE Anfangsdatum <> "" '
+        # insert_stmt += 'AND Rueckzahlung = ""'
+        insert_stmt += 'GROUP BY DkNummer '
+        stmt.execute(insert_stmt);
+        print "Anzahl: ", stmt.rowcount
+        conn.commit()
 
-    sys.exit(0)
-
-    print "Buchungen hinzufügen"
-    insert_stmt = 'INSERT INTO db_to.Buchungen (id, VertragsId, Datum, Buchungsart, Betrag) '
-    insert_stmt += 'SELECT a.BuchungId, b.Id, '
-    insert_stmt += '("20" || substr(a.Datum,7,2) || "-" || substr(a.Datum,4,2) || "-" || substr(a.Datum,1,2)), '
-    insert_stmt += 'CASE WHEN (a.Betrag >= 0) THEN 1 ELSE 2 END, a.Betrag*100 '
-    insert_stmt += 'FROM db_from.DKBuchungen a, db_to.Vertraege b'
-    insert_stmt += 'WHERE a.DkNummer <> "Stammkapital" '
-    insert_stmt += 'AND a.DkNummer = b.Kennung '
-    stmt.execute(insert_stmt);
-    print "Anzahl: ", stmt.rowcount
-    conn.commit()
-
-    sys.exit(0)
-
+        print "Buchungen hinzufügen"
+        insert_stmt = 'INSERT INTO db_to.Buchungen (id, VertragsId, Datum, Buchungsart, Betrag) '
+        insert_stmt += 'SELECT a.BuchungId, a.BuchungId, '
+        insert_stmt += '("20" || substr(a.Datum,7,2) || "-" || substr(a.Datum,4,2) || "-" || substr(a.Datum,1,2)), '
+        insert_stmt += 'CASE WHEN (a.Betrag >= 0) THEN 1 ELSE 2 END, a.Betrag*100 '
+        insert_stmt += 'FROM db_from.DKBuchungen a '
+        stmt.execute(insert_stmt);
+        print "Anzahl: ", stmt.rowcount
+        conn.commit()
         
-    print "VertragsId setzen"
-    update_stmt = 'UPDATE db_to.Buchungen a SET a.VertragsId = '
-    update_stmt += '('
-    update_stmt += 'SELECT c.id FROM db_from.DKBuchungen b, db_to.Vertraege c '
-    update_stmt += 'WHERE a.id = b.BuchungId '
-    update_stmt += 'AND b.DkNummer = c.Kennung '
-    update_stmt += ')'
+        print "VertragsId setzen"
+        update_stmt = 'UPDATE db_to.Buchungen SET VertragsId = '
+        update_stmt += '('
+        update_stmt += 'SELECT c.id FROM db_from.DKBuchungen b, db_to.Vertraege c '
+        update_stmt += 'WHERE db_to.Buchungen.id = b.BuchungId '
+        update_stmt += 'AND b.DkNummer = c.Kennung '
+        update_stmt += ')'
 
-    update_stmt = 'UPDATE db_to.Buchungen a SET a.VertragsId = '
-    update_stmt += '('
-    update_stmt += 'SELECT c.id FROM db_to.Vertraege c '
-    update_stmt += 'WHERE a.id = (SELECT b.BuchungId FROM db_from.DKBuchungen b WHERE c.id = b.BuchungId AND b.DkNummer = c.Kennung) '
-    update_stmt += 'AND c.Kennung = (SELECT b.DkNummer FROM db_from.DKBuchungen b WHERE b.DkNummer = c.Kennung) '
-    update_stmt += ')'
-
-    update_stmt = 'SELECT c.id FROM db_to.Vertraege c '
-    update_stmt += 'WHERE NOT EXISTS (SELECT b.BuchungId FROM db_from.DKBuchungen b, db_to.Vertraege c WHERE c.id = b.BuchungId AND b.DkNummer = c.Kennung) '
-    # update_stmt += 'AND c.Kennung = (SELECT b.DkNummer FROM db_from.DKBuchungen b WHERE b.DkNummer = c.Kennung) '
-
-    update_stmt = 'SELECT c.id, a.id, c.Kennung FROM db_to.Buchungen a, db_from.DKBuchungen b, db_to.Vertraege c '
-    update_stmt += 'WHERE a.id = b.BuchungId '
-    update_stmt += 'AND b.DkNummer = c.Kennung '
-
-    update_stmt = 'UPDATE db_to.Buchungen a SET a.VertragsId = '
-    update_stmt += '('
-    update_stmt += 'SELECT c.id FROM db_to.Buchungen a, db_from.DKBuchungen b, db_to.Vertraege c '
-    update_stmt += 'WHERE a.id = b.BuchungId '
-    update_stmt += 'AND b.DkNummer = c.Kennung '
-    update_stmt += ')'
-
-    update_stmt = 'UPDATE db_to.Buchungen SET VertragsId = '
-    update_stmt += '('
-    update_stmt += 'SELECT CASE WHEN (c.id IS NULL) THEN 0 ELSE c.id END FROM db_to.Vertraege c '
-    update_stmt += 'WHERE db_to.Buchungen.id = (SELECT b.BuchungId FROM db_from.DKBuchungen b WHERE c.id = b.BuchungId AND b.DkNummer = c.Kennung) '
-    update_stmt += 'AND c.Kennung = (SELECT b.DkNummer FROM db_from.DKBuchungen b WHERE b.DkNummer = c.Kennung) '
-    update_stmt += ')'
-
+        stmt.execute(update_stmt);
     
-    print "Buchungen aufsummieren"
-    update_stmt = 'UPDATE db_to.Vertraege SET Betrag='
-    update_stmt += '('
-    update_stmt += 'SELECT SUM(Betrag)*100 FROM db_from.DKBuchungen WHERE db_to.Vertraege.Kennung = db_from.DKBuchungen.DkNummer '
-    update_stmt += 'AND db_from.DKBuchungen.DkNummer <> "Stammkapital"'
-    update_stmt += 'GROUP BY db_from.DKBuchungen.DkNummer'
-    update_stmt += ')'
-    stmt.execute(update_stmt);
-    print "Anzahl: ", stmt.rowcount
+        print "Anzahl: ", stmt.rowcount
+        # vertraege = stmt.fetchall()
+        # print "vertraege", len(vertraege)
+        # for row in vertraege:
+        #     print row
+        conn.commit()
 
-    print "Rueckzahlung berücksichtigen"
-    subselect_stmt = ""
-    subselect_stmt += '('
-    subselect_stmt += 'SELECT (Betrag * -1)*100 FROM db_from.DKBuchungen WHERE db_to.Vertraege.Kennung = db_from.DKBuchungen.DkNummer '
-    subselect_stmt += 'AND (Anfangsdatum IS NULL OR Anfangsdatum = "") '
-    subselect_stmt += 'AND Betrag < 0 '
-    subselect_stmt += 'AND (substr(Rueckzahlung ,7,2) = "' + Year2 + '")'
-    subselect_stmt += 'AND DkNummer <> "Stammkapital"'
-    subselect_stmt += ')'
-
-    update_stmt = 'UPDATE db_to.Vertraege SET Betrag='
-    update_stmt += subselect_stmt
-    update_stmt += 'WHERE EXISTS '
-    update_stmt += subselect_stmt
-     
-    stmt.execute(update_stmt);
-    print "Anzahl: ", stmt.rowcount
-
-    # rows = stmt.fetchall()
-    # print "rows", len(rows)
-    # for row in rows:
-    #     print row
-
-    print "Rueckzahlungen korrigieren"
-    update_stmt = 'UPDATE db_to.Vertraege SET Betrag=0 '
-    update_stmt += 'WHERE EXISTS '
-    update_stmt += '('
-    update_stmt += 'SELECT DkNummer FROM db_from.DKBuchungen WHERE db_to.Vertraege.Kennung = db_from.DKBuchungen.DkNummer '
-    update_stmt += 'AND db_from.DKBuchungen.Rueckzahlung <> "" AND substr(db_from.DKBuchungen.Rueckzahlung,7,2) < "' + Year2 + '" '    
-    update_stmt += ') '
-    stmt.execute(update_stmt);    
-    print "Anzahl: ", stmt.rowcount
-    
-    conn.commit()
     sys.exit(0)
-        
-    print "LetzteZinsberechnung auf db_from.DKBuchungen.Datum ('2019-01-01'), wenn db_from.DKBuchungen.Datum = '01.01.19'"
-    update_stmt = 'UPDATE db_to.Vertraege SET LetzteZinsberechnung='
-    update_stmt += '('
-    update_stmt += 'SELECT ("20" || substr(Datum,7,2) || "-" || substr(Datum,4,2) || "-" || substr(Datum,1,2)) '
-    update_stmt += 'FROM db_from.DKBuchungen WHERE db_to.Vertraege.Kennung = db_from.DKBuchungen.DkNummer '
-    update_stmt += 'AND db_from.DKBuchungen.Datum = "01.01.' + Year2 + '"'
-    update_stmt += ') '
-    # update_stmt += 'WHERE db_to.Vertraege.Kennung IN (SELECT db_from.DKBuchungen.DkNummer FROM db_from.DKBuchungen WHERE db_from.DKBuchungen.Datum = "01.01.19")'
-    stmt.execute(update_stmt);
-    print "Anzahl: ", stmt.rowcount
 
-    print "db_to.Vertraege.Kennung.LetzteZinsberechnung auf db_from.DKBuchungen.Rueckzahlung ('2019-01-01'), wenn db_from.DKBuchungen.Rueckzahlung <> ''"
-    update_stmt = 'UPDATE db_to.Vertraege '
-    update_stmt += 'SET LetzteZinsberechnung='
-    update_stmt += '('
-    update_stmt += 'SELECT "20" || substr(Rueckzahlung,7,2) || "-" || substr(Rueckzahlung,4,2) || "-" || substr(Rueckzahlung,1,2) '
-    update_stmt += 'FROM db_from.DKBuchungen WHERE db_to.Vertraege.Kennung = db_from.DKBuchungen.DkNummer '
-    update_stmt += 'AND db_from.DKBuchungen.Rueckzahlung <> "" '
-    # update_stmt += 'AND db_from.DKBuchungen.Rueckzahlung IS NOT NULL'
-    update_stmt += ')'
-    # update_stmt += 'WHERE db_to.Vertraege.Kennung IN '
-    # # AND db_from.DKBuchungen.Rueckzahlung IS NOT NULL
-    # update_stmt += '(SELECT db_from.DKBuchungen.DkNummer FROM db_from.DKBuchungen WHERE db_from.DKBuchungen.Rueckzahlung <> "")'
-    stmt.execute(update_stmt);
-    print "Anzahl: ", stmt.rowcount
-
-    print "db_to.Vertraege.Kennung.LaufzeitEnde auf db_from.DKBuchungen.Rueckzahlung ('2019-01-01'), wenn db_from.DKBuchungen.Rueckzahlung <> ''"
-    update_stmt = 'UPDATE db_to.Vertraege '
-    update_stmt += 'SET Aktiv=0 AND LaufzeitEnde='
-    update_stmt += '('
-    update_stmt += 'SELECT "20" || substr(Rueckzahlung,7,2) || "-" || substr(Rueckzahlung,4,2) || "-" || substr(Rueckzahlung,1,2) '
-    update_stmt += 'FROM db_from.DKBuchungen '
-    update_stmt += 'WHERE db_to.Vertraege.Kennung = db_from.DKBuchungen.DkNummer '
-    update_stmt += 'AND db_from.DKBuchungen.Rueckzahlung <> "" '
-    update_stmt += 'GROUP BY db_from.DKBuchungen.DkNummer'    
-    update_stmt += ') '
-    update_stmt += 'WHERE db_to.Vertraege.Kennung IN '
-    update_stmt += '(SELECT db_from.DKBuchungen.DkNummer FROM db_from.DKBuchungen WHERE db_from.DKBuchungen.Rueckzahlung <> "")'
-    # print update_stmt
-    stmt.execute(update_stmt);
-    print "Anzahl: ", stmt.rowcount
-    conn.commit()
-    '''
 except SystemExit:
     pass
 except:
